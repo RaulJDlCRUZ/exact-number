440ef60f1a514d1443102b02b6abebbff5533316
azucenafbernal <azucenafbernal21@gmail.com>
2025-09-17 20:30:15 +0200

    comments

--- src/main/java/com/kangoo/cyl/CifrasYLetrasApplication.java
+++ src/main/java/com/kangoo/cyl/CifrasYLetrasApplication.java
@@ -8,23 +8,33 @@ public class CifrasYLetrasApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CifrasYLetrasApplication.class, args);
+
+		// Define target number and the six numbers to use
 		int targetNum = new com.kangoo.cyl.domain.service.Rand().randomTarget();
 		int[] numToUse = new com.kangoo.cyl.domain.service.Rand().randomNumbers();
+
+		//Show the target number and the numbers to use
 		System.out.println("Target Number: " + targetNum);
 		System.out.print("Numbers to use: ");
 		for (int i = 0; i < numToUse.length; i++) {
 			System.out.print(numToUse[i] + " ");
 		}
 		System.out.println();
+
+		// Create the arrays which will be used to store the used numbers, temporary numbers, operations and totals
 		boolean[] used = new boolean[numToUse.length];
 		int[] temp = new int[numToUse.length];
 		char[] operation = new char[numToUse.length - 1];
-		int[] totals = new int[temp.length]; // Array to store totals
+		int[] totals = new int[temp.length]; 
+
+		// Call the backtracking method with indexes and current total initialized to 0
 		new com.kangoo.cyl.domain.service.Solver().rocketscience(numToUse, targetNum, used, temp, 0, 0, operation, totals, 0);
 	}
 
 	public static void showSolution(int targetNum, int[] temp, int tempIndex, char[] operation, int[] totals, int opIndex) {
 		System.out.println("Solution found:");
+
+		// Print the operations performed to reach the target number
 		for (int i = 0; i < opIndex; i++) {
 			System.out.println(totals[i] + " " + operation[i] + " " + temp[i + 1] + " = " + totals[i + 1]);
 		}
--- src/main/java/com/kangoo/cyl/domain/service/Rand.java
+++ src/main/java/com/kangoo/cyl/domain/service/Rand.java
@@ -3,13 +3,17 @@ package com.kangoo.cyl.domain.service;
 public class Rand {
 
     public int randomTarget() {
-        return (int) (Math.random() * 900) + 101; // Random number between 101 and 999
+
+        // Random number between 101 and 999
+        return (int) (Math.random() * 900) + 101;
     }
 
     public int[] randomNumbers() {
         int[] numbers = new int[6];
+
         // Two large numbers: 25, 50, 75, 100
         int[] largeNumbers = {25, 50, 75, 100};
+
         // Four small numbers: 1 to 10
         for (int i = 0; i < 2; i++) {
             numbers[i] = largeNumbers[(int) (Math.random() * largeNumbers.length)];

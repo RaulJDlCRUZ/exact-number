8ab1754cb7a6dc283009f811d12867cc28602494
Raúl Jiménez <Raul.Jimenez8@alu.uclm.es>
2026-06-05 19:57:12 +0200

    Pom updated

--- lambda/src/main/java/com/kangoo/cyl/lambda/SolverLambdaHandler.java
+++ lambda/src/main/java/com/kangoo/cyl/lambda/SolverLambdaHandler.java
@@ -1,33 +1,49 @@
 package com.kangoo.cyl.lambda;
 
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.kangoo.cyl.application.CifrasYLetrasService;
 import com.kangoo.cyl.domain.entity.OptimalSolution;
 import com.kangoo.cyl.domain.repository.SolutionRepository;
 import com.kangoo.cyl.dto.SolveRequest;
 import com.kangoo.cyl.dto.SolveResponse;
 import com.kangoo.cyl.infrastructure.dynamodb.DynamoSolutionRepository;
-import com.amazonaws.services.lambda.runtime.RequestHandler;
-import com.amazonaws.services.lambda.runtime.Context;
-import java.util.stream.Collectors;
 
-public class SolverLambdaHandler implements RequestHandler<SolveRequest, SolveResponse> {
+public class SolverLambdaHandler implements RequestHandler<Map<String, Object>, SolveResponse> {
 
-    SolutionRepository repo = new DynamoSolutionRepository(); // ELIJO IN MEMORY PERO SI SE QUIERE USAR DYNAMO CAMBIAR
+    private final ObjectMapper mapper = new ObjectMapper();
+
+    private final SolutionRepository repo = new DynamoSolutionRepository();
+
     private final CifrasYLetrasService service = new CifrasYLetrasService(repo);
 
     @Override
-    public SolveResponse handleRequest(SolveRequest request, Context context) {
-        OptimalSolution solution = service.solveA(
-                request.getNumbers(),
-                request.getTarget());
-        SolveResponse response = new SolveResponse();
-        response.target = request.getTarget();
-        response.distance = solution.getMinimalDistance();
-        response.operationsNeeded = solution.getOperationsNeeded();
-        response.operations = solution.getOptimalSolution()
-                .stream()
-                .map(Object::toString)
-                .collect(Collectors.toList());
-        return response;
+    public SolveResponse handleRequest(Map<String, Object> input, Context context) {
+
+        try {
+            String body = (String) input.get("body");
+            SolveRequest request = mapper.readValue(body, SolveRequest.class);
+
+            OptimalSolution solution = service.solveA(
+                    request.getNumbers(),
+                    request.getTarget());
+
+            SolveResponse response = new SolveResponse();
+            response.target = request.getTarget();
+            response.distance = solution.getMinimalDistance();
+            response.operationsNeeded = solution.getOperationsNeeded();
+            response.operations = solution.getOptimalSolution()
+                    .stream()
+                    .map(Object::toString)
+                    .collect(Collectors.toList());
+
+            return response;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
